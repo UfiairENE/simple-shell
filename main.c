24157e69bb8233566bc21515830a3c8efd7c0994
Ufia Irene <89124723+UfiairENE@users.noreply.github.com>
2022-05-19 05:40:33 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * controlC - Helper to capture the signal of CTRL+C
+ * @var: Integer variable
+ */
+void controlC(int var)
+{
+	write(1, "\n($) ", 5);
+	(void) var;
+}
+
+/**
+ * main - Simple Shell Project
+ * @argc: Number of arguments - Integer
+ * @argv: Array of arguments - Array of strings
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+	int status = 0;
+
+	(void) argc;
+	signal(SIGINT, controlC);
+	status = shellLoop(argv);
+
+	return (status);
+}
